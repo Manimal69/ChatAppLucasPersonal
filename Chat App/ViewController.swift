794e1a3181c3d7e4d240d0d8000ea9d37f12c0f3
Lucas Aranda <lucasaranda@Lucass-MacBook-Pro.local>
2017-11-16 20:50:37 -0800

    first commit

---
+++ Chat App/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Chat App
+//
+//  Created by Lucas Aranda on 16/11/2017.
+//  Copyright © 2017 Citrus Circuits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var incognitomodeSwitch: UISwitch!
+    @IBOutlet weak var roomidTextField: UITextField!
+    override func viewDidLoad() {
+       
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelToChatViewController(segue:UIStoryboardSegue) {
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let navController = segue.destination as? UINavigationController {
+            let dest = navController.topViewController as! ChatRoomTableViewController
+            dest.roomKey = self.roomidTextField.text!
+            dest.username = self.usernameTextField.text!
+            dest.isIncog = self.incognitomodeSwitch.isOn
+    }
+
+    
+
+
+}
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# ChatAppLucasPersonal
-# ChatAppLucasPersonal
